8d12922a07c6c651c4e8e3e5f4b03c28abbe61b7
Axure <axurez@vip.qq.com>
2015-05-26 10:35:00 +0800

    Start to write the hexagon view.

--- HexiumGameController.swift
+++ HexiumGameController.swift
@@ -6,26 +6,26 @@
 import Foundation
 import UIKit
 
-enum Color {
-    case Red
-    case Green
-    case Yellow
-}
+//enum Color {
+//    case Red
+//    case Green
+//    case Yellow
+//}
 
 struct SingleHex {
-    let color: Color
+//    let color: Color
     let expectedSurroundings: Int
     var actualSurroudings: Int {
-        didSet {
-            switch actualSurroudings {
-                case let a where a > expectedSurroundings:
-                    color = Color.Red // Too much coupling for color here?
-                case let a where a == expectedSurroundings:
-                    color = Color.Yellow
-                case let a where a < expectedSurroundings:
-                    color = Color.Green
-            }
-        }
+//        didSet {
+//            switch actualSurroudings {
+//                case let a where a > expectedSurroundings:
+//                    color = Color.Red // Too much coupling for color here?
+//                case let a where a == expectedSurroundings:
+//                    color = Color.Yellow
+//                case let a where a < expectedSurroundings:
+//                    color = Color.Green
+//            }
+//        }
     }
 
 
@@ -170,15 +170,112 @@ class HexGameController: HexGameModelProtocol, UIViewController {
 
 
 
-}
 
+}
+// Why is it not delegated?
 class SingleHexView: UIView {
     var color: UIColor
+    var numberLabel: UILabel
+    var delegate: AppearanceProviderProtocol
+    let angleOffSet: CGFloat
+    let radius: CGFloat
+    let center: CGPoint
+    let value: Int
+
+
+    var expectedNumber: Int = 0 {
+       didSet {
+           backgroundColor = delegate.singleHexColor(expectedNumber, actualNumber: actualNumber)
+           numberLabel.textColor = delegate.singleHexTextColor(expectedNumber, actualNumber: actualNumber)
+           numberLabel.text = "\(actualNumber)"
+       }
+    }
+    var actualNumber: Int = 0
+
+    required init(coder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+
+    init(position: CGPoint, radius: CGFloat, color: UIColor, angleOffSet: CGFloat, value: Int, delegate d: AppearanceProviderProtocol) {
+        delegate = d
+
+        self.angleOffSet = angleOffSet
+        super.init(frame: CGRectMake(position.x, position.y, radius, radius))
+    }
+
+    func degreeToRadian(a: CGFloat) -> CGFloat {
+        return (CGFloat)(M_PI) / 2 * a
+    }
+
+    func polygonVertices(number: Int, point: CGPoint, radius: CGFloat, angleOffSet: CGFloat = 0) -> [CGPoint] {
+        let angleShare = degreeToRadian(360 / number)
+        let angle = angleOffSet
+        var points = [CGPoint]
+        for i in 0..<number {
+            points.append(CGPoint(point.x + radius * cos(angle), point.y + radius * sin(angle)))
+            angle += angleShare
+        }
+        return points
+    }
+
+    func drawPolygon(#ctx:CGContextRef, #x:CGFloat, #y:CGFloat, #radius:CGFloat, #sides:Int, #color:UIColor) {
+
+        let points = polygonVertices(sides, CGPoint(x, y), radius)
+
+        CGContextAddLines(ctx, points, UInt(points.count))
+
+        let cgcolor = color.CGColor
+        CGContextSetFillColorWithColor(ctx,cgcolor)
+        CGContextFillPath(ctx)
+    }
+
+    override func drawRect(rect: CGRect) {
+        let angleOff = self.angleOffSet
+        let xOffSet = CGFloat(radius)
+        var ctx = UIGraphicsGetCurrentContext()
+        drawPolygon(ctx: ctx, x: CGRectGetMidX(rect),y: CGRectGetMidY(rect),radius: CGRectGetWidth(rect)/5, sides: 6, color: UIColor.greenColor())
+    }
 
 }
 
+class HexGameboardView: UIView {
+    var dimension: Int
+
+    var singleHexRadius: CGFloat
+    var singleHexPadding: CGFloat
+    var hexes: Dictionary<(Int, Int), SingleHexView>
+
+    let provider = AppearanceProvider()
+
+    // Not ready for animations
+
+    init(dimension d: Int, singleHexRadius radius: CGFloat, singleHexPadding padding: CGFloat, backgroundColor: UIColor, foregroundColor: UIColor) {
+        assert(d > 0)
+
+        dimension = d
+        singleHexRadius = radius
+        singleHexPadding = padding
+        hexes = Dictionary()
+
+
+    }
+
+
+    func getCordinate((Int, Int)) -> CGPoint {
+
+    }
+
+
+
+}
+
+
+
+
+
 protocol AppearanceProviderProtocol: class {
-    func SingleHexColor() -> UIColor
+    func singleHexColor(expectedNumber: Int, actualNumber: Int) -> UIColor
+    func singleHexTextColor(expectedNumber: Int, actualNumber: Int) -> UIColor
 }
 
 class AppearanceProvider: AppearanceProviderProtocol {
